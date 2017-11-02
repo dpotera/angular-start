39375fc393c1d2db0d67404df843b40ef0d83693
Dominik Potera <dominik.potera@gmail.com>
2017-11-02 23:56:57 +0100

    Added Materialize CSS framework, Converted styles to SCSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
