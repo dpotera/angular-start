324286b119eea36b3ab9992c164b508eda04eb72
Dominik Potera <dominik.potera@gmail.com>
2017-11-02 22:44:46 +0100

    Tour of Heroes: Master/Detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,28 @@ export class AppComponent {
     id: 1,
     name: 'Dominik'
   };
+  selectedHero: Hero;
+  heroes = HEROES;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
 
 export class Hero {
   id: number;
   name: String;
 }
+
+const HEROES: Hero[] = [
+  { id: 1, name: 'Dominik' },
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
