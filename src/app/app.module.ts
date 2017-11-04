ebfd1cba26a8fcfe425e64a79b4e1025cf8fd199
Dominik Potera <dominik.potera@gmail.com>
2017-11-04 01:01:15 +0100

    Tour of Heroes: Multiple Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
