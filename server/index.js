d067fabecc3d5481d607ff981f352c0c77b014ad
Dominik Potera <dominik.potera@gmail.com>
2017-11-12 20:30:23 +0100

    Node.js Heroes data endpoint; Added .gitignore

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => res.send('Hello World! hehe'));
+
+const heroes = require('./data/heroes.json');
+app.get('/heroes', (req, res) => res.send(heroes));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
