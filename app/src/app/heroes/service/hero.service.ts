97a337f9b983a4233fa0a78df9d448227d2cff4b
Dominik Potera <dominik.potera@gmail.com>
2017-12-08 00:56:33 +0100

    Display heroes from mongo database

--- app/src/app/heroes/service/hero.service.ts
+++ app/src/app/heroes/service/hero.service.ts
@@ -5,21 +5,23 @@ import { HEROES } from '../mock/mock-heroes';
 import { MessageService } from '../../message/service/message.service';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class HeroService {
-  private static SERVICE_NAME: String = 'HeroService';
+  private static SERVICE_NAME = 'HeroService';
+  private heroesUrl = 'http://localhost:3000/heroes';
 
-  constructor(private messageService: MessageService) {
+  constructor(private http: HttpClient, private messageService: MessageService) {
   }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add(`${HeroService.SERVICE_NAME}: fetched heroes`);
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`${HeroService.SERVICE_NAME}: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    return this.http.get<Hero>(`${this.heroesUrl}/${id}`);
   }
 }
--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
@@ -8,13 +9,15 @@ mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017', {useMongoClient: true});
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const heroSchema = new Schema({
+    _id:  String,
     id:  String,
     name: String
 });
 const Hero = mongoose.model('Hero', heroSchema);
-const dominik = new Hero({id: 1, name: 'Dominik'});
+const dominik = new Hero({_id: 1, name: 'Dominik'});
 // dominik.save((err, dominik) => {
 //     if (err) throw err;
 //     console.log(`${dominik.name} saved in Database`);
@@ -36,16 +39,45 @@ app.get('/heroes', (req, res) => {
         console.log(heroes);
     });
 });
+
+// const heroes = require('./data/heroes.json');
+app.get('/heroes/:id', (req, res) => {
+    Hero.findOne({_id: req.params.id}, (err, heroes) => {
+        if (err) throw err;
+        res.send(heroes);
+        console.log(heroes);
+    });
+});
+
 app.post('/heroes', (req, res) => {
     console.dir(req.body);
     new Hero({
+        _id: req.body.id,
         id: req.body.id,
         name: req.body.name
     }).save((err, hero) => {
-        if (err) throw err;
-        console.log(`${hero.name} saved in Database!`);
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(`${hero.name} saved in Database!`);
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/html'});
+            res.end(err.message);
+            console.log(err.message);
+        } else {
+            console.log(`${hero.name} saved in Database!`);
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(`${hero.name} saved in Database!`);
+        }
+    })
+});
+
+app.delete('/heroes/:id', (req, res) => {
+    Hero.remove({_id: req.params.id.toString()}, (err) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/html'});
+            res.end(err.message);
+        } else {
+            console.log(`Hero id:${req.params.id} deleted from Database!`);
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(`Hero id:${req.params.id} deleted from Database!`);
+        }
     })
 });
 
